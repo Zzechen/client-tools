6231f5292bd0ebbf602e39c8736d1b6d1a30bbec
zzc <zzcm159@gmail.com>
2026-04-26 22:08:40 +0800

    fix(ios/sdk): fix margin/padding dp scale double-multiply, dispatch UI ops to main thread

--- packages/ios/sdk/Sources/ViewModify/ConstraintModifier.swift
+++ packages/ios/sdk/Sources/ViewModify/ConstraintModifier.swift
@@ -3,9 +3,6 @@ import UIKit
 class ConstraintModifier {
 
     static func modifyMargin(_ view: UIView, attribute: NSLayoutConstraint.Attribute, constant: CGFloat) {
-        let scale = UIScreen.main.scale
-        
-        // 1. 优先查找 superview 上的外部约束
         if let superview = view.superview {
             if let constraint = superview.constraints.first(where: {
                 ($0.firstItem as? UIView) === view && $0.firstAttribute == attribute
@@ -13,47 +10,41 @@ class ConstraintModifier {
                 constraint.constant += constant
                 return
             }
-
-            // 检查 secondItem
             if let constraint = superview.constraints.first(where: {
                 ($0.secondItem as? UIView) === view && $0.secondAttribute == attribute
             }) {
-                constraint.constant += constant
+                constraint.constant -= constant
                 return
             }
         }
 
-        // 2. 查找内部约束
         if let constraint = view.constraints.first(where: { $0.firstAttribute == attribute }) {
             constraint.constant += constant
             return
         }
 
-        // 3. 找不到则添加新约束
         addConstraint(to: view, attribute: attribute, constant: constant)
     }
 
     static func modifyMarginTop(_ view: UIView, diffDp: CGFloat) {
-        modifyMargin(view, attribute: .top, constant: diffDp * UIScreen.main.scale)
+        modifyMargin(view, attribute: .top, constant: diffDp)
     }
 
     static func modifyMarginBottom(_ view: UIView, diffDp: CGFloat) {
-        modifyMargin(view, attribute: .bottom, constant: diffDp * UIScreen.main.scale)
+        modifyMargin(view, attribute: .bottom, constant: -diffDp)
     }
 
     static func modifyMarginLeading(_ view: UIView, diffDp: CGFloat) {
-        modifyMargin(view, attribute: .leading, constant: diffDp * UIScreen.main.scale)
+        modifyMargin(view, attribute: .leading, constant: diffDp)
     }
 
     static func modifyMarginTrailing(_ view: UIView, diffDp: CGFloat) {
-        modifyMargin(view, attribute: .trailing, constant: diffDp * UIScreen.main.scale)
+        modifyMargin(view, attribute: .trailing, constant: -diffDp)
     }
-    
+
     private static func addConstraint(to view: UIView, attribute: NSLayoutConstraint.Attribute, constant: CGFloat) {
         guard let superview = view.superview else { return }
-        
         view.translatesAutoresizingMaskIntoConstraints = false
-        
         let constraint = NSLayoutConstraint(
             item: view,
             attribute: attribute,
--- packages/ios/sdk/Sources/ViewModify/ViewModifyService.swift
+++ packages/ios/sdk/Sources/ViewModify/ViewModifyService.swift
@@ -5,46 +5,38 @@ class ViewModifyService {
     private let viewQueryService = ViewQueryService()
 
     func modify(id: String, props: ModifyProps) -> Bool {
-        guard let view = viewQueryService.findView(byId: id) else {
-            return false
-        }
+        guard let view = viewQueryService.findView(byId: id) else { return false }
 
-        let scale = CGFloat(UIScreen.main.scale)
+        DispatchQueue.main.async {
+            if let top = props.marginTopDiffDp {
+                ConstraintModifier.modifyMarginTop(view, diffDp: CGFloat(top))
+            }
+            if let bottom = props.marginBottomDiffDp {
+                ConstraintModifier.modifyMarginBottom(view, diffDp: CGFloat(bottom))
+            }
+            if let leading = props.marginLeftDiffDp {
+                ConstraintModifier.modifyMarginLeading(view, diffDp: CGFloat(leading))
+            }
+            if let trailing = props.marginRightDiffDp {
+                ConstraintModifier.modifyMarginTrailing(view, diffDp: CGFloat(trailing))
+            }
 
-        // margin 修改
-        if let top = props.marginTopDiffDp {
-            ConstraintModifier.modifyMarginTop(view, diffDp: CGFloat(top))
-        }
-        if let bottom = props.marginBottomDiffDp {
-            ConstraintModifier.modifyMarginBottom(view, diffDp: CGFloat(bottom))
-        }
-        if let leading = props.marginLeftDiffDp {
-            ConstraintModifier.modifyMarginLeading(view, diffDp: CGFloat(leading))
-        }
-        if let trailing = props.marginRightDiffDp {
-            ConstraintModifier.modifyMarginTrailing(view, diffDp: CGFloat(trailing))
-        }
+            let hasPadding = props.paddingTopDiffDp != nil || props.paddingBottomDiffDp != nil ||
+                             props.paddingLeftDiffDp != nil || props.paddingRightDiffDp != nil
+            if hasPadding {
+                let insets = UIEdgeInsets(
+                    top: CGFloat(props.paddingTopDiffDp ?? 0),
+                    left: CGFloat(props.paddingLeftDiffDp ?? 0),
+                    bottom: CGFloat(props.paddingBottomDiffDp ?? 0),
+                    right: CGFloat(props.paddingRightDiffDp ?? 0)
+                )
+                PaddingModifier.modifyPadding(view, insets: insets)
+            }
 
-        // padding 修改
-        let hasPadding = props.paddingTopDiffDp != nil || props.paddingBottomDiffDp != nil ||
-                         props.paddingLeftDiffDp != nil || props.paddingRightDiffDp != nil
-        if hasPadding {
-            let insets = UIEdgeInsets(
-                top: CGFloat(props.paddingTopDiffDp ?? 0) * scale,
-                left: CGFloat(props.paddingLeftDiffDp ?? 0) * scale,
-                bottom: CGFloat(props.paddingBottomDiffDp ?? 0) * scale,
-                right: CGFloat(props.paddingRightDiffDp ?? 0) * scale
-            )
-            PaddingModifier.modifyPadding(view, insets: insets)
+            FrameModifier.modifyFrame(view, widthDp: props.widthDp, heightDp: props.heightDp)
+            view.setNeedsLayout()
+            view.layoutIfNeeded()
         }
-
-        // frame 修改
-        FrameModifier.modifyFrame(view, widthDp: props.widthDp, heightDp: props.heightDp)
-        
-        // 触发布局更新
-        view.setNeedsLayout()
-        view.layoutIfNeeded()
-
         return true
     }
 }
