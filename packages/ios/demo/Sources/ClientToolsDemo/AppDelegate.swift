e4804ed6a90add0e83e76cdd2543a7ea01ce3d4c
zzc <zzcm159@gmail.com>
2026-04-26 22:10:24 +0800

    fix(ios/demo): start ClientToolsSDK in AppDelegate

--- packages/ios/demo/Sources/ClientToolsDemo/AppDelegate.swift
+++ packages/ios/demo/Sources/ClientToolsDemo/AppDelegate.swift
@@ -1,9 +1,11 @@
 import UIKit
+import ClientToolsSDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        ClientToolsSDK.shared.start()
         return true
     }
 
