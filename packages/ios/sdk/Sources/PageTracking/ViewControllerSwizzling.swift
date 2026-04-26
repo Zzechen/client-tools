214b63dee2c592a0203bbe31e0a52608930e9fd3
zzc <zzcm159@gmail.com>
2026-04-26 22:09:20 +0800

    fix(ios/sdk): prevent double swizzle in ViewControllerSwizzling

--- packages/ios/sdk/Sources/PageTracking/ViewControllerSwizzling.swift
+++ packages/ios/sdk/Sources/PageTracking/ViewControllerSwizzling.swift
@@ -2,7 +2,12 @@ import UIKit
 
 class ViewControllerSwizzling {
 
+    private static var isSwizzled = false
+
     static func swizzle() {
+        guard !isSwizzled else { return }
+        isSwizzled = true
+
         let originalSelector = #selector(UIViewController.viewDidAppear(_:))
         let swizzledSelector = #selector(UIViewController.ct_viewDidAppear(_:))
 
@@ -10,16 +15,13 @@ class ViewControllerSwizzling {
               let swizzledMethod = class_getInstanceMethod(UIViewController.self, swizzledSelector) else {
             return
         }
-
         method_exchangeImplementations(originalMethod, swizzledMethod)
     }
 }
 
 extension UIViewController {
-    
     @objc func ct_viewDidAppear(_ animated: Bool) {
         self.ct_viewDidAppear(animated)
-        
         let className = String(describing: type(of: self))
         ClientToolsSDK.shared.recordPageChange(className)
     }
